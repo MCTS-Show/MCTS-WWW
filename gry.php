8837c4b1adf048db3c2b5d418683f96b6db2d6d3
MsMaciek123 <62432088+MsMaciek123@users.noreply.github.com>
2025-03-15 21:36:48 +0100

    add icons to games

--- gry.php
+++ gry.php
@@ -21,46 +21,69 @@
 	<!-- Content -->
 	<div class="container-fluid container-pixo">
 		<div class="content py-5 px-5 my-5">
-			<div class="bg-dark mb-4">
-				<h3>Sky Fight</h3>
-				<p>Sky Fight to gra rozgrywająca się na wyspach nad voidem, gdzie wszystkie drużyny rywalizują ze sobą, aby przeżyć jak najdłużej. Każdy na start dostaje podstawowe itemy, a resztę może zdobyć ze skrzynek z losowymi itemami. Na sam koniec zacznie się eliminacja, w której tnt będzie spadać z nieba, aż nie zabije wszystkich graczy. Punkty są przydzielane za zdobyte miejsce, przeżyty czas oraz za zabójstwa.</p>
+			<div class="minigame-container">
+				<img class="minigame-img" src="assets/images/minigames/skyfight.png" />
+				<div class="bg-dark mb-4 minigame-text">
+					<h3>Sky Fight</h3>
+					<p>Sky Fight to gra rozgrywająca się na wyspach nad voidem, gdzie wszystkie drużyny rywalizują ze sobą, aby przeżyć jak najdłużej. Każdy na start dostaje podstawowe itemy, a resztę może zdobyć ze skrzynek z losowymi itemami. Na sam koniec zacznie się eliminacja, w której tnt będzie spadać z nieba, aż nie zabije wszystkich graczy. Punkty są przydzielane za zdobyte miejsce, przeżyty czas oraz za zabójstwa.</p>
+				</div>
 			</div>
-			<div class="bg-dark mb-4">
-				<h3>Parkour</h3>
-				<p>Parkour to klasyczna gra, w której wyczucie skoku odgrywa kluczową role. Mapa jest podzielona na rundy oraz etapy: łatwy, średni, trudny, bardzo trudny. Na każdą runde przypada tylko jedna próba, więc jeśli gracz spadnie traci życie. Po stracie wszystkich żyć odpada. Punkty są przydzielane za zdobyte miejsce.</p>
+			<div class="minigame-container">
+				<img class="minigame-img" src="assets/images/minigames/parkour.png" />
+				<div class="bg-dark mb-4 minigame-text">
+					<h3>Parkour</h3>
+					<p>Parkour to klasyczna gra, w której wyczucie skoku odgrywa kluczową role. Mapa jest podzielona na rundy oraz etapy: łatwy, średni, trudny, bardzo trudny. Na każdą runde przypada tylko jedna próba, więc jeśli gracz spadnie traci życie. Po stracie wszystkich żyć odpada. Punkty są przydzielane za zdobyte miejsce.</p>
+				</div>
 			</div>
-			<div class="bg-dark mb-4">
-				<h3>Infection</h3>
-				<p>W trybie Infection kluczowa jest celność i zwinność. Łuk to ważna broń, ale są też inne opcje, jak potki do ataku i obrony. Gracze posiadają butle, które leczą drużynę i służą do otwierania sejfu z punktami. Więcej butli przyspiesza usuwanie zainfekowanej warstwy blokującej sejf, ale butle można niszczyć, co przywraca warstwę. Gracze zamiast umierać, zostają zainfekowani i muszą być uleczoni, by przeżyć. Mapa zawiera system wentylacji do szybkiego, ukrytego przemieszczania się, a z czasem bramy otwierają nowe pokoje. Na końcu w ostatnim pokoju poziom lawy rośnie, a punkty zdobywa się za pozycję, zabójstwa i wykopane bloki.</p>
+			<div class="minigame-container">
+				<img class="minigame-img" src="assets/images/minigames/infection.png" />
+				<div class="bg-dark mb-4 minigame-text">
+					<h3>Infection</h3>
+					<p>W trybie Infection kluczowa jest celność i zwinność. Łuk to ważna broń, ale są też inne opcje, jak potki do ataku i obrony. Gracze posiadają butle, które leczą drużynę i służą do otwierania sejfu z punktami. Więcej butli przyspiesza usuwanie zainfekowanej warstwy blokującej sejf, ale butle można niszczyć, co przywraca warstwę. Gracze zamiast umierać, zostają zainfekowani i muszą być uleczoni, by przeżyć. Mapa zawiera system wentylacji do szybkiego, ukrytego przemieszczania się, a z czasem bramy otwierają nowe pokoje. Na końcu w ostatnim pokoju poziom lawy rośnie, a punkty zdobywa się za pozycję, zabójstwa i wykopane bloki.</p>
+				</div>
 			</div>
-			<div class="bg-dark mb-4">
-				<h3>Circle Rush</h3>
-				<p>Circle Rush to gra rozgrywająca się na okrągłej mapie w dwuosobowych składach. Gracze na tym trybie muszą zająć jak najwięcej stref i przy tym nie dać się zabić, ponieważ odradzanie jest wyłączone. Punkty są przydzielane za przejęte strefy oraz za zabójstwa.</p>
+			<div class="minigame-container">
+				<img class="minigame-img" src="assets/images/minigames/circlerush.png" />
+				<div class="bg-dark mb-4 minigame-text">
+					<h3>Circle Rush</h3>
+					<p>Circle Rush to gra rozgrywająca się na okrągłej mapie w dwuosobowych składach. Gracze na tym trybie muszą zająć jak najwięcej stref i przy tym nie dać się zabić, ponieważ odradzanie jest wyłączone. Punkty są przydzielane za przejęte strefy oraz za zabójstwa.</p>
+				</div>
 			</div>
-			<div class="bg-dark mb-4">
-				<h3>Deadly Run</h3>
-				<p>Deadly Run to zabójczy wyścig, w którym gracze muszą przejść specjalnie przygotowane tory. Dodatkowo mają możliwość walki między sobą, co może mocno utrudnić grę. Punkty są przydzielane za zdobyte miejsce.</p>
+			<div class="minigame-container">
+				<img class="minigame-img" src="assets/images/minigames/deadlyrun.png" />
+				<div class="bg-dark mb-4 minigame-text">
+					<h3>Deadly Run</h3>
+					<p>Deadly Run to zabójczy wyścig, w którym gracze muszą przejść specjalnie przygotowane tory. Dodatkowo mają możliwość walki między sobą, co może mocno utrudnić grę. Punkty są przydzielane za zdobyte miejsce.</p>
+				</div>
 			</div>
-			<div class="bg-dark mb-4">
-				<h3>Survival Games</h3>
-				<p>Survival Games to gra, która stawia przed graczami wyzwanie przetrwania na ogromnej mapie ze skrzynkami wypełnionymi losowym lootem, a drużyna, która przeżyje jako ostatnia, wygrywa. Pvp włącza się po kilkunastu sekundach od startu, a punkty są przydzielane za zdobyte miejsce, przeżyty czas oraz za zabójstwa.</p>
+			<div class="minigame-container">
+				<img class="minigame-img" src="assets/images/minigames/survivalgames.png" />
+				<div class="bg-dark mb-4 minigame-text">
+					<h3>Survival Games</h3>
+					<p>Survival Games to gra, która stawia przed graczami wyzwanie przetrwania na ogromnej mapie ze skrzynkami wypełnionymi losowym lootem, a drużyna, która przeżyje jako ostatnia, wygrywa. Pvp włącza się po kilkunastu sekundach od startu, a punkty są przydzielane za zdobyte miejsce, przeżyty czas oraz za zabójstwa.</p>
+				</div>
 			</div>
-            <div class="bg-dark mb-4">
-                <h3>Explosion Tag</h3>
-                <p>Explosion Tag to wybuchowy tryb, w którym gracz posiadający tnt musi sie go czym predzej pozbyc, jesli nie zrobi tego odpowiednio szybko, wybuchnie i zostanie wyeliminowany. Gracze przekazuja miedzy soba tnt uderzajac się, a osoba ktora przetrwa jako ostatnia wygrywa. Punkty są przydzielane za zdobyte miejsce.</p>
-            </div>
-            <div class="bg-dark mb-4">
-                <h3>Capture The Flag</h3>
-                <p>Capture The Flag to gra, w której gracze muszą zdobyć flagę przeciwników i przenieść ją do swojej bazy. Przed grą gracze mogą rozstawić pułapki, w które mogą wpaść przeciwnicy. Po śmierci gracze się odradzają a punkty są przydzielane za doniesione flagi oraz zdobyte miejsce.</p>
-            </div>
-            <div class="bg-dark mb-4">
-                <h3>Hole In The Wall</h3>
-                <p>Na Hole In The Wall trzeba mieć oczy dookoła głowy. Zabójcze ściany tylko czekają na to, aby zepchnąć z platformy nieświadomych graczy! Ściany mogą pojawiać się z każdej strony, a ich prędkość i trudność zwiększa się z czasem! Dodatkowo trzeba uważać na znikające bloki na platformie oznaczone kolorem czerwonym. Punkty są przydzielane na przeżyty czas i zdobyte miejsce.</p>
-            </div>
-            <div class="bg-dark">
-                <h3>???</h3>
-                <p></p>
-            </div>
+			<div class="minigame-container">
+				<img class="minigame-img" src="assets/images/minigames/explosiontag.png" />
+				<div class="bg-dark mb-4 minigame-text">
+					<h3>Explosion Tag</h3>
+					<p>Explosion Tag to wybuchowy tryb, w którym gracz posiadający tnt musi sie go czym predzej pozbyc, jesli nie zrobi tego odpowiednio szybko, wybuchnie i zostanie wyeliminowany. Gracze przekazuja miedzy soba tnt uderzajac się, a osoba ktora przetrwa jako ostatnia wygrywa. Punkty są przydzielane za zdobyte miejsce.</p>
+				</div>
+			</div>
+			<div class="minigame-container">
+				<img class="minigame-img" src="assets/images/minigames/capturetheflag.png" />
+				<div class="bg-dark mb-4 minigame-text">
+					<h3>Capture The Flag</h3>
+					<p>Capture The Flag to gra, w której gracze muszą zdobyć flagę przeciwników i przenieść ją do swojej bazy. Przed grą gracze mogą rozstawić pułapki, w które mogą wpaść przeciwnicy. Po śmierci gracze się odradzają a punkty są przydzielane za doniesione flagi oraz zdobyte miejsce.</p>
+				</div>
+			</div>
+			<div class="minigame-container">
+				<img class="minigame-img" src="assets/images/minigames/holeinthewall.png" />
+				<div class="bg-dark mb-4 minigame-text">
+					<h3>Hole In The Wall</h3>
+					<p>Na Hole In The Wall trzeba mieć oczy dookoła głowy. Zabójcze ściany tylko czekają na to, aby zepchnąć z platformy nieświadomych graczy! Ściany mogą pojawiać się z każdej strony, a ich prędkość i trudność zwiększa się z czasem! Dodatkowo trzeba uważać na znikające bloki na platformie oznaczone kolorem czerwonym. Punkty są przydzielane na przeżyty czas i zdobyte miejsce.</p>
+				</div>
+			</div>
 		</div>
 	</div>
 
